5e2fb269ff2f252f09dcaf17d157241722669d32
Nafiz Imtiaz Khan <nafiz.ba.pakizatvl@gmail.com>
2025-01-20 20:46:24 +0600

    ADO.NET with Stored Procedure Done

--- Test ADO/NEC/NEC.API/obj/Debug/net8.0/NEC.API.AssemblyInfo.cs
+++ Test ADO/NEC/NEC.API/obj/Debug/net8.0/NEC.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NEC.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+240260b148ec0f00f92b909115d24da4f8080a4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6f70cafc6f317d770f90b31c2970db75e9b6f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NEC.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NEC.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
